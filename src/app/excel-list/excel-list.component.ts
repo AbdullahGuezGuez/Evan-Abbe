519fe98d468f08cecdb4374232f40cc7b1c30a3b
Saboo <evan.saboo@sogeti.com>
2019-11-22 14:35:48 +0100

    push to abbe :)

--- src/app/excel-list/excel-list.component.ts
+++ src/app/excel-list/excel-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';  
+import { Observable } from 'rxjs';
+import * as xlsx from 'xlsx'; 
 
 
 @Component({
@@ -20,13 +21,29 @@ export class ExcelListComponent implements OnInit {
 
   }  
   
+  changeListener($event): void {
+    this.handleFileInput($event.target);
+  }
+  handleFileInput(inputValue: any) {
+    var file: File = inputValue.files[0];
+    var reader: FileReader = new FileReader();
+    reader.readAsArrayBuffer(file);
+    reader.onloadend =  (e) =>{
+      var buffer = reader.result;
+      var bytes = new Uint8Array(<ArrayBuffer>buffer);
+      var wb = xlsx.read(bytes, {type:'array'});
+      var worksheet = wb.Sheets["Prospect att bemanna"];
+
+     var worksheetJson = xlsx.utils.sheet_to_json(worksheet);
+     console.log(worksheetJson);
+     this.excel = worksheetJson;
+    }
+
+
+   // var workbook = xlsx.readFile(this.fileToUpload.name);
+
+    
   
-  handleFileInput(files: FileList) {
-
-    this.fileToUpload = files.item(0);
-
-  console.log(this.fileToUpload);
-
 }
 
 }
